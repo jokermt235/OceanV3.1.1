7ad704c8ef68c0193a166666a94b47f07a815dc1
mutalip <jokermt2357@gmail.com>
2020-05-20 18:12:14 +0600

    Code confirm view design

--- app/src/main/java/com/example/oceanv311/View/CodeConfirmActivity.java
+++ app/src/main/java/com/example/oceanv311/View/CodeConfirmActivity.java
@@ -15,7 +15,12 @@ import com.google.firebase.auth.PhoneAuthProvider;
 public class CodeConfirmActivity extends AppCompatActivity {
 
     private Button sendCodeBtn;
-    private EditText codeConfirm;
+    private EditText codeConfirm1;
+    private EditText codeConfirm2;
+    private EditText codeConfirm3;
+    private EditText codeConfirm4;
+    private EditText codeConfirm5;
+    private EditText codeConfirm6;
     private String verificationId;
 
     @Override
@@ -23,14 +28,19 @@ public class CodeConfirmActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_code_confirm);
         sendCodeBtn = findViewById(R.id.codeConfirmLogin);
-        codeConfirm = findViewById(R.id.codeConfirmCode);
+        codeConfirm1 = findViewById(R.id.codeConfirmCode1);
+        codeConfirm2 = findViewById(R.id.codeConfirmCode2);
+        codeConfirm3 = findViewById(R.id.codeConfirmCode3);
+        codeConfirm4 = findViewById(R.id.codeConfirmCode4);
+        codeConfirm5 = findViewById(R.id.codeConfirmCode5);
+        codeConfirm6 = findViewById(R.id.codeConfirmCode6);
 
         verificationId = getIntent().getStringExtra("verificationId");
 
         sendCodeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                PhoneAuthCredential phoneAuthCredential = PhoneAuthProvider.getCredential(verificationId, codeConfirm.getText().toString());
+                PhoneAuthCredential phoneAuthCredential = PhoneAuthProvider.getCredential(verificationId, codeConfirm1.getText().toString());
                 Auth.signInWithPhoneCredentials(CodeConfirmActivity.this, phoneAuthCredential);
             }
         });
