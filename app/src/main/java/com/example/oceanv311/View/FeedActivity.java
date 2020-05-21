0c6b9e3edb6fa7abd6a5a7f6d88efaf0309e665b
Mateo <joker1988_88@mail.com>
2020-05-22 00:55:43 +0600

    Adding action to menu

--- app/src/main/java/com/example/oceanv311/View/FeedActivity.java
+++ app/src/main/java/com/example/oceanv311/View/FeedActivity.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import com.example.oceanv311.R;
 
 public class FeedActivity extends AppCompatActivity {
     private RecyclerView feedPostView;
+    private ImageButton addPost;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
